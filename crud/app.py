3a3382eeb90f0ac5138d00954576d190f15a1163
Rogerio Santos <rogeriosblack@gmail.com>
2017-08-01 16:26:05 -0300

    Primeiro commit Iniciando os estudos

--- crud/app.py
+++ crud/app.py
@@ -32,5 +32,29 @@ db.create_all()
 def index():
     return render_template("index.html")
 
+@app.route("/cadastrar")
+def cadastrar():
+    return render_template("cadastro.html")
+
+@app.route("/cadastro", methods=['GET', 'POST'])
+def cadastro():
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        telefone = request.form.get("telefone")
+        cpf = request.form.get("cpf")
+        email = request.form.get("email")
+
+        if nome and telefone and cpf and email:
+            p = Pessoa(nome, telefone, cpf, email)
+            db.session.add(p)
+            db.session.commit()
+
+    return redirect(url_for("index"))
+
+@app.route("/lista")
+def lista():
+    pessoas = Pessoa.query.all()
+    return render_template("lista.html", pessoas=pessoas)
+
 if __name__=='__main__':
     app.run(debug=True)
