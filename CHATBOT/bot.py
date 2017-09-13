e0b82cd0f0bbacbbf41ba4bcad5c61178dd5949e
Rogerio Santos <rogeriosblack@gmail.com>
2017-09-13 09:55:59 -0300

    Primeiro commit Iniciando chat

--- CHATBOT/bot.py
+++ CHATBOT/bot.py
@@ -16,6 +16,14 @@ chats = ['hi', 'oi', 'hello', 'how are you?', 'I''m fine.', 'Thanks.'] # convers
 bot.set_trainer(ListTrainer) # Define o metodo de treinamento
 bot.train(chats) # define a lista de conversas
 
+r = sr.Recognizer()
+
+with sr.Microphone() as s:
+    r.adjust_for_ambient_noise(s)
+
 while True:
+    audio = r.listen(s)
+    speech = r.recognize_google(audio)
+
     request = input('Enter text: ')
     print(bot.get_response(request))
